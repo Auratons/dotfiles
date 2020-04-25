d58c7e5ba12de0efd371345c5edeae453522431b
Tomas Kremel <kremelt@gmail.com>
2020-04-25 19:42:44 +0200

    Pip completion support added.

--- .oh-my-zsh-custom/misc.zsh
+++ .oh-my-zsh-custom/misc.zsh
@@ -6,3 +6,16 @@ alias jobs='jobs -l'
 alias tensorboard='tensorboard --logdir .'
 alias reload='source ~/.zshrc'
 alias la='ls -alFh'
+
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] 2>/dev/null ))
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
+
