936a56ec0e60a1b07525712d8565768386b617ed
Tomas Kremel <kremelt@gmail.com>
2020-10-29 22:55:32 +0100

    Git aliases and configuration updated.

--- .oh-my-zsh-custom/git.zsh
+++ .oh-my-zsh-custom/git.zsh
@@ -7,9 +7,11 @@ alias gco='git commit'
 alias grm='git rm'
 alias gce='git checkout'
 alias gba='git branch -a'
-alias glg='git log --branches --remotes --tags --graph --oneline --decorate'
+alias glg="git log --all --color --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %C(auto)%s%C(reset) %Cgreen(%cr) %C(bold blue)%an'"
 alias gsm='gst -suno | sed -E "s/[[:blank:]]?M[[:blank:]]//"'
 alias grh='git reset HEAD'
+alias gu='git fetch --all --tags --prune  &&  git merge --ff-only @{u}'
+alias gud='git fetch --all --tags --prune  &&  git rebase origin/develop'
 
 # Git Flow aliases
 alias gff='git flow feature'
